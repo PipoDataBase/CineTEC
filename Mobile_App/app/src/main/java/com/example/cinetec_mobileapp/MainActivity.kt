563d3a74e4dc0482301ad428ad7a979c8fb404d5
Jose Andrés Rodríguez Rojas <joseandres216@gmail.com>
2023-09-21 21:08:17 -0600

    Secon main commit of android app

--- Mobile_App/app/src/main/java/com/example/cinetec_mobileapp/MainActivity.kt
+++ Mobile_App/app/src/main/java/com/example/cinetec_mobileapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.cinetec_mobileapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,18 @@ class MainActivity : AppCompatActivity() {
             val goToMovieListingScreen: Intent = Intent(this, movieListing:: class.java)
             startActivity(goToMovieListingScreen)
         }
+
+        val spnLocations: Spinner = findViewById(R.id.spnUbicacion)
+
+        val locationsOptions = arrayOf("Cartago", "Perez Zeledon", "San Carlos")
+
+        spnLocations.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, locationsOptions)
+
+        val spnBranchOffices: Spinner = findViewById(R.id.spnSucursal)
+
+        val branchOfficesOptions = arrayOf("Paseo metropoli", "San Isidro", "La Fortuna")
+
+        spnBranchOffices.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, branchOfficesOptions)
+
     }
 }
