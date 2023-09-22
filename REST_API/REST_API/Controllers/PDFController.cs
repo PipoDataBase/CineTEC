3f676f75fe99dedfa245c039d8522d706dd1e949
CAMANEM <camanem10@hotmail.com>
2023-09-21 21:10:45 -0600

    Implemented PDF Generation
    Implementation of PDF Generation from URL

---
+++ REST_API/REST_API/Controllers/PDFController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace REST_API.Controllers
+{
+    
+    [ApiController]
+    [Route("GeneratedPDFfromURL")]
+    public class PDFController : ControllerBase
+    {
+        private async Task<ActionResult> DownloadFile (string filePath) 
+        {
+            var Downloadfilepath = Path.Combine(Directory.GetCurrentDirectory(), "GeneratedPdfFiles", filePath);
+
+            var provider = new FileExtensionContentTypeProvider();
+            if (!provider.TryGetContentType(Downloadfilepath, out var contentType)) 
+            {
+                contentType = "application/octet-stream";
+            }
+            var bytes = await System.IO.File.ReadAllBytesAsync(Downloadfilepath);
+            return File(bytes, contentType, Path.GetFileName(Downloadfilepath));
+        }
+
+        [HttpGet]
+        [Route("GeneratePDFfromURL")]
+        public async Task<IActionResult> GeneratePDFfromURL(string URLlink)
+        {
+            var renderer = new ChromePdfRenderer();
+
+
+            var pdf = renderer.RenderUrlAsPdf(URLlink);
+
+
+            string PDFfilename = DateTime.Now.Ticks.ToString() + ".pdf";
+            pdf.SaveAs(Path.Combine(Directory.GetCurrentDirectory(), "GeneratedPdfFiles", PDFfilename));
+
+            return await DownloadFile(PDFfilename);
+        }
+
+    }
+}
--- REST_API/REST_API/Program.cs
+++ REST_API/REST_API/Program.cs
@@ -1,33 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using REST_API;
 using REST_API.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<CineTECDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString")));
+var app = Startup.InitializeApp(args);
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
